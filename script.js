0c64e7360095c7d3af1c3bf597b91000f39297f1
vincaf <vincenzocafassog@gmail.com>
2022-07-13 17:30:33 +0200

    added dynamic generation of 10 mails

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const app = new Vue(
 
         data: {
             mailGenerated: '',
+            listMailGenerated: [],
         },
 
         methods:{
             generateNewMail(){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then( (response) => {
-                    this.mailGenerated = response.data.response;
-                });
+                for (let i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then( (response) => {
+                        this.mailGenerated = response.data.response;
+                        this.listMailGenerated.push(this.mailGenerated);
+                    });
+                };
+                console.log(listMailGenerated);
             }
         },
 
