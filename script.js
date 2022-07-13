f923e49c6f2329a1c8d83f26c275e5a240b67f30
vincaf <vincenzocafassog@gmail.com>
2022-07-13 16:37:27 +0200

    initialization of Vue and console.log single mail

---
+++ script.js
@@ -0,0 +1,20 @@
+const app = new Vue(
+    {
+        el: '#app',
+
+        data: {
+            mailGenerated: '',
+        },
+
+        methods:{
+
+        },
+
+        created(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                console.log(response.data.response);
+            });
+        }
+    }
+);
